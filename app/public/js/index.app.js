1fc6bc173666b379993165d607447f4bda622274
natehopf-evil <70525401+natehopf-evil@users.noreply.github.com>
2020-09-13 10:45:05 -0400

    Create index.app.js
    adding in js documentation

---
+++ app/public/js/index.app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el: '#begginer1',
+  data: {
+    message: 'Hello Vue!',
+    oneuser:{
+      "firstname":"Bossman",
+      "lastname":"Pit",
+      "origin":"Mars",
+      "birthdate":"3/27/1604",
+      "age":"416",
+    }
+  }
+})
