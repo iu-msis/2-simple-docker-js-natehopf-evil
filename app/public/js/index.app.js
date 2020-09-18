fdae8b29184330fba0e0425a171cf589f8bd5b85
natehopf-evil <70525401+natehopf-evil@users.noreply.github.com>
2020-09-18 07:50:12 -0400

    Update index.app.js
    writting jscript to pull in JSON data

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -1,13 +1,34 @@
 var app = new Vue({
-  el: '#begginer1',
-  data: {
-    message: 'Hello Vue!',
-    oneuser:{
-      "firstname":"Bossman",
-      "lastname":"Pit",
-      "origin":"Mars",
-      "birthdate":"3/27/1604",
-      "age":"416",
-    }
-  }
+el: '#userProfile',
+data: {
+userName:'',
+userLast:'',
+userEmail:'',
+userCountry:'',
+userDob:'',
+userAge:'',
+userImage:'',
+userImgThumb:''
+},
+created(){
+this.fetchUser();
+},
+
+methods:{
+fetchUser:function(){
+fetch('https://randomuser.me/api/')
+.then(response => response.json())
+.then(data => {
+var userData = data.results[0];
+console.log(userData);
+this.userName = userData.name.first;
+this.userLast = userData.name.last;
+this.userDob = userData.dob.date;
+this.userAge = userData.dob.age;
+this.userEmail = 'mailto:' +userData.email;
+this.userCountry = userData.nat;
+this.userImage = userData.picture.large;
+this.userImgThumg = userData.picture.thumbnail;
 })
+}
+}})
