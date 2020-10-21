c2f3049323e8f25e18d38e3dcefd8fda91d27db3
natehopf-evil <70525401+natehopf-evil@users.noreply.github.com>
2020-10-21 10:39:37 -0400

    Update comments.app.js
    HW 7

--- app/public/js/comments.app.js
+++ app/public/js/comments.app.js
@@ -1,61 +1,23 @@
 var app = new Vue({
-  el: '#triagePage',
+  el: '#Commentsjs',
   data: {
-    ptList: [],
-    visitList: [],
-    activePt: null,
-    triageForm: {},
-    newPtForm: {}
-  },
-  computed: {
-    activePtName() {
-      return this.activePt ? this.activePt.lastName + ', ' + this.activePt.firstName : ''
+    Comments: [],
+    newComment: {
     }
   },
-  methods: {
-    newPtData() {
-      return {
-        firstName: "",
-        lastName: "",
-        dob: "",
-        sexAtBirth: ""
-      }
-    },
-    newTriageData() {
-      return {
-        priority: "",
-        visitDate: "",
-        visitDescription: ""
-      }
-    },
-    dateSince(d) {
-      // Uses Luxon date API (see comment in index.html file)
-      return moment.utc(d).local().calendar();
-    },
-    age(d) {
-      return moment().diff(moment(d), 'years');
-    },
-    /**
-     * Given a priority, returns triage class
-     * or "" if not found
-     **/
-    priorityClass(p) {
-      const priorityClass = {
-        low: "triageMinor",
-        medium: "triageUrgent",
-        high: "triageCritical"
-      };
-
-      return p in priorityClass ? priorityClass[p] : "";
-    },
-    handleNewPtForm( evt ) {
-      // evt.preventDefault();  // Redundant w/ Vue's submit.prevent
-
-      // TODO: Validate the data!
-
-      fetch('api/records/post.php', {
+methods:{
+  fetchUser:function(){
+    fetch('api/comments/')
+    .then(response => response.json())
+    .then(json => {
+      this.Comments=json;
+      console.log(this.Comments);
+    });
+  },
+  createComment(){
+      fetch('api/comments/create.php',{
         method:'POST',
-        body: JSON.stringify(this.newPtForm),
+        body: JSON.stringify(this.newComment),
         headers: {
           "Content-Type": "application/json; charset=utf-8"
         }
@@ -64,59 +26,20 @@ var app = new Vue({
       .then( json => {
         console.log("Returned from post:", json);
         // TODO: test a result was returned!
-        this.ptList.push(json[0]);
-        this.newPtForm = this.newPtData();
+        this.Comments.push(json[0]);
+        this.newComment = this.newCommentData();
       });
 
       console.log("Creating (POSTing)...!");
-      console.log(this.newPtForm);
+      console.log(this.newComment);
     },
-    handleTriageForm( evt ) {
-      console.log("Triage form submitted!");
-
-      if (!this.activePt) {
-        alert("ERROR: No patient selected!");
-        return false;
+    newCommentData(){
+      return{
+        commentText:""
       }
-      this.triageForm.patientGuid = this.activePt.patientGuid;
-
-      var tempTime = this.triageForm.visitDate == "" ? moment() : moment(this.triageForm.visitDate);
-      this.triageForm.visitDateUtc = tempTime.utc().format('YYYY-MM-DD HH:mm:ss');
-      console.log(this.triageForm);
-
-      fetch('api/visits/create.php', {
-        method:'POST',
-        body: JSON.stringify(this.triageForm),
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        }
-      })
-      .then( response => response.json() )
-      .then( json => {
-        console.log("Returned from triage create:", json);
-        this.visitList = json;
-        this.newTriageForm = this.newTriageData();
-      });
     }
-  },
-  created() {
-    fetch("api/records/")
-    .then( response => response.json() )
-    .then( json => {
-      this.ptList = json;
-
-      console.log(json)}
-    );
-
-    fetch("api/visits/")
-    .then( response => response.json() )
-    .then( json => {
-      this.visitList = json;
-
-      console.log(json)}
-    );
-
-    this.newPtForm = this.newPtData();
-    this.newTriageForm = this.newTriageData();
-  }
-})
+},
+created(){
+  this.fetchUser();
+}
+});
